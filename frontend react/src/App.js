808d27aa75ec383caeb9bbbb18b7b8e52c80e81e
psergicv <sv.postolachi@gmail.com>
2024-05-20 19:57:41 +0300

    App.js changed

--- frontend react/src/App.js
+++ frontend react/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import TaskList from './components/TaskList';
+import Footer from './components/Footer'; 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <TaskList />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
